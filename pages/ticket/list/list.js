efbac2753a5cc3139d29633fbbbc8245e78c7dbf
CrabR <961619261@qq.com>
2019-01-25 17:06:25 +0800

    购票列表修改

--- pages/ticket/list/list.js
+++ pages/ticket/list/list.js
@@ -10,13 +10,36 @@ Page({
         "id": 1,
         "title": "全国通用卷",
         "tips": "可用于观赏乐园内所有花卉园区",
-        "price": 100
+        "price": 100,
+        "src": "../resources/image/01.jpg"
       },
       {
         "id": 2,
-        "title": "全国通用卷",
-        "tips": "这是标题二",
-        "price": 60
+        "title": "自选园区票",
+        "tips": "可用于观赏购买的自选园区的花卉",
+        "price": 42,
+        "src": "../resources/image/02.jpg"
+      }, 
+      {
+        "id": 3,
+        "title": "四季花卉园区",
+        "tips": "可用于观赏四季花卉园区的花卉",
+        "price": 26,
+        "src": "../resources/image/03.jpg"
+      },
+      {
+        "id": 4,
+        "title": "荷兰花卉园区",
+        "tips": "可用于观赏荷兰花卉园区的花卉",
+        "price": 40,
+        "src": "../resources/image/04.jpg"
+      },
+      {
+        "id": 5,
+        "title": "俄罗斯花卉园区",
+        "tips": "可用于观赏俄罗斯花卉园区的花卉",
+        "price": 32,
+        "src": "../resources/image/05.jpg"
       }
     ]
   },
